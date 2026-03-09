6e75325762844ae8a340c109dc3d34fb769375c6
venia-maker <venia@suiteop.com>
2026-03-09 14:18:22 +0100

    feat(next.config): add rewrites for webhook handling
    - Implemented rewrites in the Next.js configuration to route '/webhooks/:path*' to '/api/webhooks/:path*', enhancing the API's webhook handling capabilities.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  async rewrites() {
+    return [
+      {
+        source: '/webhooks/:path*',
+        destination: '/api/webhooks/:path*',
+      },
+    ]
+  },
 };
 
 export default nextConfig;
